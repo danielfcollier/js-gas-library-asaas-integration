faa232aef62b5987c5e3735c18b77054febb4e00
Daniel Collier <danielfcollier@gmail.com>
2023-07-30 11:14:31 -0300

    chore: adjust valid config

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,8 @@ const urlSandbox = "https://sandbox.asaas.com";
 
 const TEST_ENVIRONMENT = "SANDBOX"; // PRODUCTION
 const TEST_API_KEY =
-  "$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MNWZmNDQ6OjAwMDAwMDAwMDAwMDAwMDU1Mzk6OiRhYWNoXzE4ZjUyYzE1LTM1MGYtNDhmMi04NmNkLTNlZjBjMmZjYTNlMw==";
+  // eslint-disable-next-line
+  "$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAwMDU1Mzk6OiRhYWNoXzE4ZjUyYzE1LTM1MGYtNDhmMi04NmNkLTNlZjBjMmZjYTNlMw==";
 
 function getBaseUrl(config) {
   return config.isProduction ? urlProduction : urlSandbox;

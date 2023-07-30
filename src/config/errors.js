632f9747c61171221a43a9c34cc90e9c6ec976cb
Daniel Collier <danielfcollier@gmail.com>
2023-07-30 11:18:48 -0300

    style: fix lint

--- src/config/errors.js
+++ src/config/errors.js
@@ -2,28 +2,59 @@ const AsaasIntegrationError = { name: "AsaasIntegrationError" };
 
 const ERRORS = {
   CUSTOMER: {
-    CREATE: (customer) => new Error(`Could not create customer! ${customer?.name} - ${customer?.documentId}`, AsaasIntegrationError),
-    UPDATE: (customer) => new Error(`Could not update customer! ${customer?.name} - ${customer?.documentId}`, AsaasIntegrationError),
-    DELETE: (customer) => new Error(`Could not delete customer! ${customer?.name} - ${customer?.documentId}`, AsaasIntegrationError),
+    CREATE: (customer) => {
+      const message = `Could not create customer! ${customer?.name} - ${customer?.documentId}`;
+      return new Error(message, AsaasIntegrationError);
+    },
+    UPDATE: (customer) => {
+      const message = `Could not update customer! ${customer?.name} - ${customer?.documentId}`;
+      return new Error(message, AsaasIntegrationError);
+    },
+    DELETE: (customer) => {
+      const message = `Could not delete customer! ${customer?.name} - ${customer?.documentId}`;
+      return new Error(message, AsaasIntegrationError);
+    },
   },
   PAYMENT: {
     CREATE: (payment) => {
-      new Error(`Could not create payment! ${payment?.id} - ${payment?.description}`, AsaasIntegrationError);
+      const message = `Could not create payment! ${payment?.id} - ${payment?.description}`;
+      return new Error(message, AsaasIntegrationError);
     },
-    UPDATE: (payment) => new Error(`Could not update payment! ${payment?.id} - ${payment?.description}`, AsaasIntegrationError),
-    DELETE: (payment) => new Error(`Could not delete payment! ${payment?.id} - ${payment?.description}`, AsaasIntegrationError),
+    UPDATE: (payment) => {
+      const message = `Could not update payment! ${payment?.id} - ${payment?.description}`;
+      return new Error(message, AsaasIntegrationError);
+    },
+    DELETE: (payment) => {
+      const message = `Could not delete payment! ${payment?.id} - ${payment?.description}`;
+      return new Error(message, AsaasIntegrationError);
+    },
   },
   INSTALLMENT: {
-    CREATE: (payment) =>
-      new Error(`Could not create installment! Payment: ${payment?.id} - ${payment?.description}`, AsaasIntegrationError),
-    UPDATE: (payment) =>
-      new Error(`Could not update installment! Payment: ${payment?.id} - ${payment?.description}`, AsaasIntegrationError),
-    DELETE: (payment) =>
-      new Error(`Could not delete installment! Payment: ${payment?.id} - ${payment?.description}`, AsaasIntegrationError),
+    CREATE: (payment) => {
+      const message = `Could not create installment! Payment: ${payment?.id} - ${payment?.description}`;
+      return new Error(message, AsaasIntegrationError);
+    },
+    UPDATE: (payment) => {
+      const message = `Could not update installment! Payment: ${payment?.id} - ${payment?.description}`;
+      return new Error(message, AsaasIntegrationError);
+    },
+    DELETE: (payment) => {
+      const message = `Could not delete installment! Payment: ${payment?.id} - ${payment?.description}`;
+      return new Error(message, AsaasIntegrationError);
+    },
   },
   RECEIPT: {
-    CREATE: (payment) => new Error(`Could not create receipt! Payment: ${payment?.id} - ${payment?.description}`, AsaasIntegrationError),
-    UPDATE: (payment) => new Error(`Could not update receipt! Payment: ${payment?.id} - ${payment?.description}`, AsaasIntegrationError),
-    DELETE: (payment) => new Error(`Could not delete receipt! Payment: ${payment?.id} - ${payment?.description}`, AsaasIntegrationError),
+    CREATE: (payment) => {
+      const message = `Could not create receipt! Payment: ${payment?.id} - ${payment?.description}`;
+      return new Error(message, AsaasIntegrationError);
+    },
+    UPDATE: (payment) => {
+      const message = `Could not update receipt! Payment: ${payment?.id} - ${payment?.description}`;
+      return new Error(message, AsaasIntegrationError);
+    },
+    DELETE: (payment) => {
+      const message = `Could not delete receipt! Payment: ${payment?.id} - ${payment?.description}`;
+      return new Error(message, AsaasIntegrationError);
+    },
   },
 };
